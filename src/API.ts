a49e8dfe4e9c954bb0aa9aaceec972bdea876506
janiapurva <jani.apurva@yahoo.com>
2021-05-22 21:42:59 -0400

    successful fetching data from api

--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,32 @@
+import { shuffleArray } from "./utils";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
   EASY = "easy",
   MEDIAM = "mediam",
-  HARD = "hard"
+  HARD = "hard",
 }
-export const fetchQuizQuestions = async (amount:number,difficulty:Difficulty) => {
-  const endPoint =  `http://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-  const data = await(await fetch(endPoint)).json()
-  console.log(data);
-}
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endPoint = `http://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endPoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
